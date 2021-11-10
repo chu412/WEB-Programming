9f17d875ce3fb50080b521b7c71c3bfe5ec4d667
chu412 <62446650+chu412@users.noreply.github.com>
2021-11-10 11:48:29 +0300

    Update script.js

--- static/script/script.js
+++ static/script/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
-    newText = document.createTextNode("Loaded in " + loadTime.toString() + " ms");
+    var newText = document.createTextNode("Loaded in " + loadTime.toString() + " ms");
 	document.getElementsByTagName("FOOTER")[0].appendChild(newText);
 
     
