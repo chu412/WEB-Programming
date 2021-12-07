d974b9c21973acb014ee68da9e99789cfcebfa01
chu412 <62446650+chu412@users.noreply.github.com>
2021-12-08 02:46:02 +0300

    Add files via upload

---
+++ static/script/request.js
@@ -0,0 +1,40 @@
+
+window.addEventListener('load', () => {
+
+    let id = 1;
+    const preloader = document.getElementById("preloader");
+    const posts = document.getElementById("posts");
+    const button = document.getElementById("add-post");
+    button.addEventListener('click', async function (e) {
+        button.disabled = true;
+        const post_t = document.getElementById("one-post");
+        const post = post_t.content.cloneNode(true);
+        const post_title = post.querySelector("h3");
+        const post_body = post.getElementById("post-body");
+        preloader.classList.remove('disabled');
+        try {
+            let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+            if (response.ok) {
+                let json = await response.json();
+                console.log(json);
+                if (Object.keys(json).length) {
+                    post_title.innerHTML = json.title;
+                    post_body.setAttribute('id', id);
+                    post_body.innerHTML = json.body;
+                    id = id + 1;
+                } else {
+                    alert("⚠ Something went wrong!");
+                }
+                preloader.classList.add('disabled');
+                posts.insertBefore(post, preloader);
+            } else {
+                alert("⚠ Something went wrong!");
+            }
+            button.disabled = false;
+        } catch (error) {
+            alert("⚠ Something went wrong!");
+            preloader.classList.add('disabled');
+            button.disabled = false;
+        }
+    });
+})
